eee7c7c2cc04b6c4e5f13c9769fa60355ce7b123
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2022-01-13 10:47:38 -0500

    Added the functional mapper for User and UserDto

--- biblioteca-admin-usuarios/src/main/java/dev/j3c/mspractice/collection/helpers/ContactData.java
+++ biblioteca-admin-usuarios/src/main/java/dev/j3c/mspractice/collection/helpers/ContactData.java
@@ -1,15 +1,16 @@
 package dev.j3c.mspractice.collection.helpers;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@builder(toBuilder = true)
+@Builder(toBuilder = true)
 public class ContactData {
     private String address;
-    private String phoneNumber;
     private String email;
+    private String phoneNumber;
 }
--- biblioteca-admin-usuarios/src/main/java/dev/j3c/mspractice/dto/helpers/ContactDataDto.java
+++ biblioteca-admin-usuarios/src/main/java/dev/j3c/mspractice/dto/helpers/ContactDataDto.java
@@ -5,10 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class ContactDataDto {
-
+    @NotBlank
+    private String address;
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String phoneNumber;
 }
