0de3ddffac31a8517acca68c36f10473ac1d90c4
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2022-01-13 10:09:17 -0500

    Created ms for users administration

---
+++ biblioteca-admin-usuarios/src/main/java/dev/j3c/mspractice/BibliotecaAdminUsuariosApplication.java
@@ -0,0 +1,13 @@
+package dev.j3c.mspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaAdminUsuariosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotecaAdminUsuariosApplication.class, args);
+    }
+
+}
