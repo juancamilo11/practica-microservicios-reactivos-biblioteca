465b2de257c7bfd9390fa05986095ba3e5e99afc
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2022-01-13 23:14:38 -0500

    Created the microservice for facturation

---
+++ biblioteca-facturacion/src/main/java/dev/j3c/mspractice/BibliotecaFacturacionApplication.java
@@ -0,0 +1,13 @@
+package dev.j3c.mspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaFacturacionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotecaFacturacionApplication.class, args);
+    }
+
+}
