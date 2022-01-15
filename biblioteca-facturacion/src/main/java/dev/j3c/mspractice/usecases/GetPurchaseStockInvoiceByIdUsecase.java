e8fd4827bd04bab9378ed794816d25ed7bd35c63
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2022-01-15 01:58:49 -0500

    Implemented interfaces for get invoice by id

--- biblioteca-facturacion/src/main/java/dev/j3c/mspractice/usecases/GetPurchaseStockInvoiceByIdUsecase.java
+++ biblioteca-facturacion/src/main/java/dev/j3c/mspractice/usecases/GetPurchaseStockInvoiceByIdUsecase.java
@@ -1,7 +1,34 @@
 package dev.j3c.mspractice.usecases;
 
+import dev.j3c.mspractice.dto.PurchaseStockInvoiceDto;
+import dev.j3c.mspractice.dto.StockInvoiceDto;
+import dev.j3c.mspractice.mapper.PurchaseInvoiceMapper;
+import dev.j3c.mspractice.repository.PurchaseStockInvoiceRepository;
 import dev.j3c.mspractice.usecases.interfaces.GetPurchaseStockInvoiceById;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Mono;
 
+@Service
+@Validated
 public class GetPurchaseStockInvoiceByIdUsecase implements GetPurchaseStockInvoiceById {
+    private final PurchaseStockInvoiceRepository invoicesRepository;
+    private final PurchaseInvoiceMapper purchaseInvoiceMapper;
 
+    @Autowired
+    public GetPurchaseStockInvoiceByIdUsecase(PurchaseStockInvoiceRepository invoicesRepository, PurchaseInvoiceMapper purchaseInvoiceMapper) {
+        this.invoicesRepository = invoicesRepository;
+        this.purchaseInvoiceMapper = purchaseInvoiceMapper;
+    }
+
+    @Override
+    public Mono<PurchaseStockInvoiceDto> apply(String id) {
+        return invoicesRepository
+                .findById(id)
+                .switchIfEmpty(Mono.error(new IllegalArgumentException("Error, la factura con id " + id + " no existe en el sistema.")))
+                .map(invoice -> purchaseInvoiceMapper
+                        .mapFromEntityToDto()
+                        .apply(invoice));
+    }
 }
--- biblioteca-facturacion/src/main/java/dev/j3c/mspractice/usecases/GetSellStockInvoiceByIdUsecase.java
+++ biblioteca-facturacion/src/main/java/dev/j3c/mspractice/usecases/GetSellStockInvoiceByIdUsecase.java
@@ -1,6 +1,36 @@
 package dev.j3c.mspractice.usecases;
 
+import dev.j3c.mspractice.dto.PurchaseStockInvoiceDto;
+import dev.j3c.mspractice.dto.SellStockInvoiceDto;
+import dev.j3c.mspractice.mapper.PurchaseInvoiceMapper;
+import dev.j3c.mspractice.mapper.SellInvoiceMapper;
+import dev.j3c.mspractice.repository.PurchaseStockInvoiceRepository;
+import dev.j3c.mspractice.repository.SellStockInvoiceRepository;
+import dev.j3c.mspractice.usecases.interfaces.GetSellStockInvoiceById;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Mono;
 
-public class GetSellStockInvoiceByIdUsecase {
+@Service
+@Validated
+public class GetSellStockInvoiceByIdUsecase implements GetSellStockInvoiceById {
+    private final SellStockInvoiceRepository sellStockInvoiceRepository;
+    private final SellInvoiceMapper sellInvoiceMapper;
 
+    @Autowired
+    public GetSellStockInvoiceByIdUsecase(SellStockInvoiceRepository sellStockInvoiceRepository, SellInvoiceMapper sellInvoiceMapper) {
+        this.sellStockInvoiceRepository = sellStockInvoiceRepository;
+        this.sellInvoiceMapper = sellInvoiceMapper;
+    }
+
+    @Override
+    public Mono<SellStockInvoiceDto> apply(String id) {
+        return sellStockInvoiceRepository
+                .findById(id)
+                .switchIfEmpty(Mono.error(new IllegalArgumentException("Error, la factura con id " + id + " no existe en el sistema.")))
+                .map(invoice -> sellInvoiceMapper
+                        .mapFromEntityToDto()
+                        .apply(invoice));
+    }
 }
