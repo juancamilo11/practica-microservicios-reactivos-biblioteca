a3b7fc6131ca54036fa8d1ed145eb199866d979d
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2022-01-15 13:04:01 -0500

    Added entrypoints for some usecases

--- biblioteca-facturacion/src/main/java/dev/j3c/mspractice/router/StockInvoiceRouter.java
+++ biblioteca-facturacion/src/main/java/dev/j3c/mspractice/router/StockInvoiceRouter.java
@@ -1,8 +1,12 @@
 package dev.j3c.mspractice.router;
 
+import dev.j3c.mspractice.dto.PurchaseStockInvoiceDto;
+import dev.j3c.mspractice.dto.SellStockInvoiceDto;
 import dev.j3c.mspractice.dto.StockInvoiceDto;
 import dev.j3c.mspractice.usecases.GetAllPurchasePurchaseStockInvoicesUsecase;
+import dev.j3c.mspractice.usecases.GetAllSellStockInvoicesUsecase;
 import dev.j3c.mspractice.usecases.GetPurchaseStockInvoiceByIdUsecase;
+import dev.j3c.mspractice.usecases.GetSellStockInvoiceByIdUsecase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -17,23 +21,42 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 @Configuration
 public class StockInvoiceRouter {
 
-//    @Bean
-//    public RouterFunction<ServerResponse> getStockInvoiceByIdRoute(GetPurchaseStockInvoiceByIdUsecase getPurchaseStockInvoiceByIdUsecase) {
-//        return route(GET("/get-invoice/{id}")
-//                .and(accept(MediaType.APPLICATION_JSON)), request -> ServerResponse
-//                .ok()
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .body(BodyInserters.fromPublisher(getPurchaseStockInvoiceByIdUsecase.apply(request.pathVariable("id")), StockInvoiceDto.class)));
-//    }
-//
-//    @Bean
-//    public RouterFunction<ServerResponse> getAllStockInvoicesRoute(GetAllPurchasePurchaseStockInvoicesUsecase getAllUsersUsecase) {
-//        return route(GET("/get-all-stock-invoices")
-//                .and(accept(MediaType.APPLICATION_JSON)), request ->
-//                ServerResponse
-//                        .ok()
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .body(BodyInserters.fromPublisher(getAllUsersUsecase.get(),StockInvoiceDto.class)));
-//    }
+    @Bean
+    public RouterFunction<ServerResponse> getAllPurchaseStockInvoicesRoute(GetAllPurchasePurchaseStockInvoicesUsecase getAllPurchaseStockInvoicesUsecase) {
+        return route(GET("/get-all-purchase-stock-invoices")
+                .and(accept(MediaType.APPLICATION_JSON)), request ->
+                ServerResponse
+                        .ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(getAllPurchaseStockInvoicesUsecase.get(),PurchaseStockInvoiceDto.class)));
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> getAllSellStockInvoicesRoute(GetAllSellStockInvoicesUsecase getAllSellStockInvoicesUsecase) {
+        return route(GET("/get-all-sell-stock-invoices")
+                .and(accept(MediaType.APPLICATION_JSON)), request ->
+                ServerResponse
+                        .ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(getAllSellStockInvoicesUsecase.get(),SellStockInvoiceDto.class)));
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> getPurchaseStockInvoiceByIdRoute(GetPurchaseStockInvoiceByIdUsecase getPurchaseStockInvoiceByIdUsecase) {
+        return route(GET("/get-purchase-invoice/{id}")
+                .and(accept(MediaType.APPLICATION_JSON)), request -> ServerResponse
+                .ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromPublisher(getPurchaseStockInvoiceByIdUsecase.apply(request.pathVariable("id")), PurchaseStockInvoiceDto.class)));
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> getSellStockInvoiceByIdRoute(GetSellStockInvoiceByIdUsecase getSellStockInvoiceByIdUsecase) {
+        return route(GET("/get-sell-invoice/{id}")
+                .and(accept(MediaType.APPLICATION_JSON)), request -> ServerResponse
+                .ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromPublisher(getSellStockInvoiceByIdUsecase.apply(request.pathVariable("id")), SellStockInvoiceDto.class)));
+    }
 
 }
