d3d63027181756c82d3309ca3b9f074ea1c92e9f
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2022-01-16 11:29:42 -0500

    Created microservice for lending resources

---
+++ biblioteca-admin-prestamos/src/main/java/dev/j3c/BibliotecaAdminPrestamosApplication.java
@@ -0,0 +1,13 @@
+package dev.j3c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaAdminPrestamosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotecaAdminPrestamosApplication.class, args);
+    }
+
+}
---
+++ biblioteca-admin-prestamos/src/test/java/dev/j3c/BibliotecaAdminPrestamosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.j3c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaAdminPrestamosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
