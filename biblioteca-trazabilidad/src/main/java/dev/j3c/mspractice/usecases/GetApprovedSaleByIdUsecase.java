dfce5e4b36e0ed2e6426ee37d50039a9bea03590
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2022-01-17 00:49:52 -0500

    Added usecases for get approved sales records

--- biblioteca-trazabilidad/src/main/java/dev/j3c/mspractice/usecases/GetApprovedSaleByIdUsecase.java
+++ biblioteca-trazabilidad/src/main/java/dev/j3c/mspractice/usecases/GetApprovedSaleByIdUsecase.java
@@ -1,17 +1,34 @@
 package dev.j3c.mspractice.usecases;
 
+import dev.j3c.mspractice.dto.ResourceLoaningDto;
+import dev.j3c.mspractice.mapper.ResourceLoaningMapper;
+import dev.j3c.mspractice.repository.ResourceLoaningRepository;
 import dev.j3c.mspractice.usecases.interfaces.GetApprovedSaleById;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Mono;
 
 @Service
 @Validated
 public class GetApprovedSaleByIdUsecase implements GetApprovedSaleById {
 
+    private final ResourceLoaningRepository resourceLoaningRepository;
+    private final ResourceLoaningMapper resourceLoaningMapper;
 
+    @Autowired
+    public GetApprovedSaleByIdUsecase(ResourceLoaningRepository resourceLoaningRepository, ResourceLoaningMapper resourceLoaningMapper) {
+        this.resourceLoaningRepository = resourceLoaningRepository;
+        this.resourceLoaningMapper = resourceLoaningMapper;
+    }
 
     @Override
-    public Flux<> apply(String id) {
-        return null;
+    public Mono<ResourceLoaningDto> apply(String id) {
+        return this.resourceLoaningRepository
+                .findById(id)
+                .switchIfEmpty(Mono.empty())
+                .map(resourceLoaning -> this.resourceLoaningMapper
+                        .mapFromEntityToDto()
+                        .apply(resourceLoaning));
     }
 }
--- biblioteca-trazabilidad/src/main/java/dev/j3c/mspractice/usecases/interfaces/GetApprovedSaleById.java
+++ biblioteca-trazabilidad/src/main/java/dev/j3c/mspractice/usecases/interfaces/GetApprovedSaleById.java
@@ -1,6 +1,9 @@
 package dev.j3c.mspractice.usecases.interfaces;
 
+import dev.j3c.mspractice.dto.ResourceLoaningDto;
+import reactor.core.publisher.Mono;
+
 @FunctionalInterface
 public interface GetApprovedSaleById {
-    Flux<> apply(String id);
+    Mono<ResourceLoaningDto> apply(String id);
 }
